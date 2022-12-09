6d322429116a4c57c4249942e8f95329d061a9d1
Gary Miller <miller.garym@gmail.com>
2022-12-09 14:47:57 +1300

    highlight syntax error

--- editor/src/components/Editor.tsx
+++ editor/src/components/Editor.tsx
@@ -40,8 +40,30 @@ export const Editor = () => {
 
 		model.onDidChangeContent((e: monaco.editor.IModelContentChangedEvent) => {
 			const m = match(model.getValue());
-			console.log("onDidChangeContent", e, model.getValue(), m.failed());
+			// console.log("onDidChangeContent", e, model.getValue(), m.failed());
+			if ( m.failed() ) {
+				console.log( "syntax error - message : ", m.message );
+				console.log( "syntax error - short message : ", m.shortMessage );
+				const errorRE = /Line (\d*), col (\d*): expected (.*)/
+				const er = errorRE.exec(m.shortMessage ? m.shortMessage : "")
+				if( er !== null ) {
+					const line = Number(er[1])
+					const col = Number(er[2])
+					const msg = er[3]
+					monaco.editor.setModelMarkers(model, "asdf", [
+						{
+							severity: 8,
+							message: msg,
+							startLineNumber: line,
+							startColumn: col,
+							endLineNumber: line,
+							endColumn: col,
+						}
+					])
+				}
+			}
 			if (m.succeeded()) {
+				monaco.editor.setModelMarkers(model, "asdf", [])
 				const module = makeAST(m);
 				setAstJsonStr(JSON.stringify(module, null, 4));
 			}
